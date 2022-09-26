c53360162985a0d8b704d57d4f8fbb66699a86fd
0papi <evanskwofie67@gmail.com>
2022-09-26 21:40:32 +0000

    feat(ui): create new form component

---
+++ components/data.js
@@ -0,0 +1,21 @@
+export const topicData = []
+export const targetStudents = []
+export const classRequirementsData = []
+export const instructorsData = [
+    {
+        id: 1,
+        name: "Nathan Stack"
+    },
+    {
+        id: 2,
+        name: "Jennifer Adanna"
+    },
+    {
+        id: 3,
+        name: "Collins Ndubisi"
+    },
+    {
+        id: 4,
+        name: "Solomon Akogun"
+    }
+]
